1d0f5e8132fe14e61bc572a4a1baf64077e466bf
_knaut <info@knaut.net>
2017-10-29 19:35:13 -0700

    isomorphism achieved

---
+++ src/Components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'react-router-dom/Link';
+
+const Navigation = () => {
+  return (
+    <ul>
+      <li>
+        <button><Link to='/'>Home</Link></button>
+      </li>
+      <li>
+        <button><Link to='/about'>About</Link></button>
+      </li>
+      <li>
+        <button><Link to='/contact'>Contact</Link></button>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
