85d9cc87c0d002f72570cb64f56a77a18513d7b5
_knaut <info@knaut.net>
2017-10-31 18:10:05 -0700

    configurable head tag

--- src/utils/renderHead.js
+++ src/utils/renderHead.js
@@ -1,15 +1,21 @@
+import HeadConfig from '../../configureHead.js';
+
 export default function renderHead( html, preloadedState ) {
   return `
-    <!doctype html>
+    <!doctype ${ HeadConfig.lang ? 'lang="' + HeadConfig.lang + '"' : null }>
     <html>
       <head>
         <title>universalBust</title>
+        ${ HeadConfig.charset ? '<meta charset="' + HeadConfig.charset '">' : null }
+        ${ HeadConfig.description ? '<meta name="description" content="' + HeadConfig.description '">' : null }
+        ${ HeadConfig.keywords ? '<meta name="keywords" content="' + HeadConfig.keywords '">' : null }
+        ${ HeadConfig.viewport ? '<meta name="viewport" content="' + HeadConfig.viewport '">' : null }
+        ${ HeadConfig.viewport ? '<meta name="viewport" content="' + HeadConfig.viewport '">' : null }
+        ${ HeadConfig.extStylesheets ? '<link rel="stylesheets" type="text/css" href="' + HeadConfig.extStylesheets '">' : null }
       </head>
       <body>
         <div id="root">${html}</div>
         <script>
-          // WARNING: See the following for security issues around embedding JSON in HTML:
-          // http://redux.js.org/docs/recipes/ServerRendering.html#security-considerations
           window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
         </script>
         <script type="text/javascript" src="/assets/bundle.js"></script>
